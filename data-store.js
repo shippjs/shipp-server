bf6b1b0f5745cde8281e52ec11c9760be8b5bc8c
Brandon Carl <brandon.j.carl@gmail.com>
2016-02-01 17:08:03 -0500

    data-store: new #insertVariables function

--- data-store.js
+++ data-store.js
@@ -14,6 +14,35 @@ var fs    = require("fs"),
 
 /*
 
+  function insertVariables
+
+*/
+
+function insertVariables(query, context) {
+
+  if (!query.filters) return query;
+
+  var query = _.clone(query),
+      key,
+      keys = _.keys(context),
+      re   = new RegExp("<(" + keys.join("|") + ")>", "gi"),
+      match;
+
+  for (key in query.filters || {}) {
+    if (_.isString(query.filters[key]) && (match = query.filters[key].match(re))) {
+      match = match[0];
+      query.filters[key] = query.filters[key].replace(new RegExp(match, "g"), context[match.slice(1, match.length-1)]);
+    }
+  }
+
+  return query;
+
+}
+
+
+
+/*
+
   function extendDatabase
 
   Adds a "get" function so that lowdb can handle key/values.
@@ -29,9 +58,10 @@ function extendDatabase(db) {
   }
 
   // Runs a database query
-  db.query = function(query) {
+  db.query = function(query, context) {
 
-    var chain = _(db.get(query.route).value());
+    var query = _.isEmpty(context) ? query : insertVariables(query, context),
+        chain = _(db.get(query.route).value());
 
     // Filters
     if (query.filters) chain = chain.filter(query.filters);
@@ -49,10 +79,10 @@ function extendDatabase(db) {
   }
 
   // Executes multiple queries and combines into a single object
-  db.queries = function(queries) {
+  db.queries = function(queries, context) {
     if (!Array.isArray(queries)) queries = [queries];
     return _.reduce(queries, function(results, query) {
-      var result = db.query(query);
+      var result = db.query(query, context);
       if ("undefined" !== typeof query.idx) result = result[query.idx];
       return _.extend(results, (query.key) ? pair([[query.key, result]]) : result);
     }, {});
