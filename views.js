0ae5d14ce51327a1c7b8766a248d8cddfd38d469
Brandon Carl <brandon.j.carl@gmail.com>
2016-02-01 12:13:48 -0500

    views: uses superloader engines

--- views.js
+++ views.js
@@ -8,51 +8,20 @@
 var Utils     = require("./utils"),
     Metadata  = require("./metadata"),
     fs        = require("fs"),
-    cons      = require("consolidate"),
     _         = require("lodash"),
     express   = require("express"),
-    Promise   = require("bluebird"),
-    readFile  = Promise.promisify(fs.readFile);
+    Motors    = require("superloader").engines;
 
 
 function addEngines(router, engines) {
-  for (var ext in engines)
-    router.engine(ext, makeEngine(engines[ext]));
+  for (var ext in engines) {
+    Motors.addEngine(ext, engines[ext])
+    router.engine(ext, Motors.compileFile.bind(Motors));
+  }
 
   // Add standard html engine if not already added
-  if (!router.engines["html"]) router.engine("html", makeEngine("html"));
-}
-
-
-function makeEngine(pipeline) {
-
-  // Simple temlating
-  if (-1 === pipeline.indexOf(">") && "html" !== pipeline) return cons[pipeline];
-
-  return function(path, options, fn) {
-
-    var promise = readFile(path, "utf8"),
-        engines = (pipeline) ? pipeline.split(">") : [],
-        engine;
-
-    // Chain through engines
-    while (engine = engines.shift())
-      if ("html" !== engine) (function(eng) {
-        promise = promise.then(function(str) { return cons[eng].render(str, options); })
-      })(engine);
-
-    // Return result
-    promise.then(function(str) { fn(null, str ); });
-
-  };
-}
-
-
-function addHtmlEngine(router) {
-  if ("undefined" === typeof router.engines["html"]) {
-    router.engine("html", function (path, options, fn) {
-      fn(null, fs.readFileSync(path, "utf8"));
-    });
+  if (!router.engines["html"]) {
+    router.engine("html", Motors.compileFile.bind(Motors));
   }
 }
 
