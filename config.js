84b57fc61a615d9778b0b10a627b4d0ddf5ac3a2
Brandon Carl <brandon.j.carl@gmail.com>
2016-02-22 08:18:20 -0500

    config: better commenting

--- config.js
+++ config.js
@@ -1,34 +1,50 @@
 
-/*
+/**
 
   Config.js
 
-*/
+  Base configuration file for sneakers server.
+
+**/
 
 var Utils  = require("./utils"),
     config = require(Utils.makePathAbsolute("sneakers.config.js"));
 
 
-// Things like extensions and mime types should be automated
 var defaults = {
+
+  // Fonts are treated as statics, the extensions help to keep our "watch" efficient
   fonts: [
     { path : "./fonts",   url : "/fonts",   ext : ["ttf", "otf", "eot", "woff", "svg"] }
   ],
+
+  // Images are static too. We do not use extensions here as there are too many types of
+  // images, video and audio that developers may place in this folder.
   images: [
     { path : "./images",  url : "/images"  }
   ],
+
+  // Scripts are bundled by default, forcing directories with an "index" script to webpack
   scripts: [
     { path : "./scripts", url : "/scripts", ext : "js", bundleFolders : true },
   ],
+
+  // Meanwhile, styles don't use bundling
   styles: [
     { path : "./styles",  url : "/styles",  ext : "css" }
   ],
+
+  // Vendor directories may include scripts, styles and JSON. As a result, we presume
+  // that these are precompiled and treat them as statics.
   statics: [
     { path : "./vendor",  url : "/vendor" }
   ],
+
+  // Views are compiled using template engines (when appropriate), HTML otherwise
   views: [
     { path : "./views",   url : "/", ext : "html" }
   ]
+
 };
 
 
@@ -39,7 +55,10 @@ module.exports = function() {
 
   // Store global variables
   global.vars = config.globals || {};
-  if (/^prod/i.test(process.env.NODE_ENV))
+
+  // Copy production variables over second, so as to overwrite non-production
+  // when applicable
+  if (Utils.isProduction())
     global.vars = Object.assign(global.vars, config["globals:production"] || {});
 
   return config;
