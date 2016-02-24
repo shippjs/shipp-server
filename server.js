f82fb56af3fb22d2fd042ff3c446e5097652dfa0
Brandon Carl <brandon.j.carl@gmail.com>
2016-02-23 20:41:14 -0500

    server: switches to new routing format

--- server.js
+++ server.js
@@ -16,28 +16,36 @@ module.exports = function() {
       server   = require("express")(),
       compiler = require("./compiler"),
       statics  = require("./statics"),
+      options,
       PORT;
 
-  // Set up sensible logging defaults, etc. These will change with production environments
   PORT = global.ports.proxy;
+
+  // Set up sensible logging defaults, etc. These will change with production environments
   server.use(require("morgan")("dev"));
   server.use(require("cookie-parser")());
   server.use(require("express-session")({ secret : "password123", resave : false, saveUninitialized : true }));
 
-  // Middleware helper
-  function iterateMiddleware(arr, middleware) {
-    arr.forEach(function(options) { server.use(middleware(options)); });
+  // Routing middleware
+  for (var route in global.config.routes) {
+
+    // Copy options and add in route
+    options = Object.assign({}, global.config.routes[route]);
+    options.url = route;
+
+    switch (options.type) {
+      case "scripts":
+      case "styles":
+      case "views":
+        server.use(compiler(options));
+        break;
+      case "statics":
+        server.use(statics(options));
+        break;
+      default:
+        throw new Error("Unrecognized route type", options.type);
+    }
   }
-
-  // Add routers
-  iterateMiddleware(global.config.views, compiler);
-
-  // Add compile
-  iterateMiddleware(global.config.styles, compiler);
-  iterateMiddleware(global.config.scripts, compiler);
-
-  // Add statics
-  iterateMiddleware(global.config.statics, statics);
 
   // We must add the data last or it overwrites other paths
   server.use(require("./data-server")());
