9fd33403fc0a58fd70243e8ad836a2ffa3f29423
Brandon Carl <brandon.j.carl@gmail.com>
2016-02-23 22:06:11 -0500

    globals: fixes comments

--- globals.js
+++ globals.js
@@ -1,5 +1,5 @@
 
-/*
+/**
 
   Globals.js
 
@@ -10,7 +10,7 @@
   • fs        File System (via MemoryFS)
   • server    Server (via BrowserSync)
 
-*/
+**/
 
 var dataStore = require("./data-store");
 
