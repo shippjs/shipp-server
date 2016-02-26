c00dad89d14c15cd038c200d6090510d1cad92a4
Brandon Carl <brandon.j.carl@gmail.com>
2016-02-26 13:30:07 -0500

    compiler: adds params

--- compiler.js
+++ compiler.js
@@ -160,7 +160,7 @@ function addFile(router, route, file, type, basePath) {
         handler  = createHandler(type, compiler, metadata);
 
     // Add routes to router
-    Utils.makeRoutes(route, file, { type : type }).forEach(function(r) {
+    Utils.makeRoutes(route, file, { type : type, params : metadata.params }).forEach(function(r) {
       router.get(r, handler);
     });
 
