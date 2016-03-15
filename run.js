c5ed4ff8b90a00ce0d23a71175600b67c8429701
Brandon Carl <brandon.j.carl@gmail.com>
2016-03-15 19:10:30 -0400

    run: removes production requirement (vestige of combined repo)

--- run.js
+++ run.js
@@ -5,8 +5,4 @@
 
 **/
 
-// For the moment, we are forcing a production environment. To use a
-// non-production environment, please use the CLI.
-process.env.NODE_ENV = "production";
-
 require("./lib/")();
