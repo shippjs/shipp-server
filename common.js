44b46301faf5d46ce2c2c0057c7e8c81a32dbea9
Brandon Carl <brandon.j.carl@gmail.com>
2016-03-15 14:32:54 -0400

    common: new file abstracting commonly used server functions

---
+++ common.js
@@ -0,0 +1,44 @@
+/**
+
+  common.js
+
+  Loads common middleware such as cookie-parsing and session management
+
+**/
+
+var fs = require("fs"),
+    favicon = require("serve-favicon"),
+    Utils = require("./utils");
+
+
+module.exports = function() {
+
+  var router = global.express.Router(),
+      bodyParser = require("body-parser"),
+      faviconFile = Utils.makePathAbsolute(global.config.favicon || "/favicon.ico");
+
+  // Logging
+  if (!Utils.isProduction())
+    router.use(require("morgan")("dev"));
+
+  // Favicon (ensures that it exists)
+  try {
+    fs.lstatSync(file);
+    router.use(favicon(faviconFile));
+  } catch (err) {};
+
+  // Cookie parsing
+  router.use(require("cookie-parser")())
+
+  // Express sessions
+  router.use(require("express-session")({ secret : "password123", resave : false, saveUninitialized : true }));
+
+  // Body parsing
+  router.use(bodyParser.urlencoded({ extended: false }));
+  router.use(bodyParser.json());
+
+  return router;
+
+};
+
+
