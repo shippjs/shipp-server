d0d4eb1552d45bbfc66edb9e4be97cc47a13910c
Brandon Carl <brandon.j.carl@gmail.com>
2021-09-19 14:33:50 -0500

    dev.sh: enables node inspect

--- dev.sh
+++ dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it --name shipp-server -v $PWD:/home/app -p 3000:3000 shipp-server
+docker run --rm -it --name shipp-server -v $PWD:/home/app -p 3000:3000 -p 9229:9229 shipp-server
