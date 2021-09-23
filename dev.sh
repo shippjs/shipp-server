af828477842a69a7f51c9241aca64423857d3528
Brandon Carl <brandon.j.carl@gmail.com>
2021-09-23 15:24:46 -0500

    Updates docker dev script to provide access to pipemaker and compilers

--- dev.sh
+++ dev.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-docker run --rm -it --name shipp-server -v $PWD:/home/app -p 3000:3000 -p 9229:9229 shipp-server
+docker run \
+--rm -it \
+--name shipp-server \
+-v $PWD:/home/app \
+-v /Users/B/Repos/Shipp/pipemaker:/home/pipemaker \
+-v /Users/B/Repos/Shipp/compilers:/home/compilers \
+-p 3000:3000 \
+-p 9229:9229 \
+shipp-server
