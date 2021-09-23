77a6576220c28e2415b4dcb396b8a157f696ad1c
Brandon Carl <brandon.j.carl@gmail.com>
2021-09-23 15:29:18 -0500

    Bumps superloader and adds to docker dev container

--- dev.sh
+++ dev.sh
@@ -5,6 +5,7 @@ docker run \
 --name shipp-server \
 -v $PWD:/home/app \
 -v /Users/B/Repos/Shipp/pipemaker:/home/pipemaker \
+-v /Users/B/Repos/Shipp/superloader:/home/superloader \
 -v /Users/B/Repos/Shipp/compilers:/home/compilers \
 -p 3000:3000 \
 -p 9229:9229 \
